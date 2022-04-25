0e91051f7eb0dc144986de1a093478c58106a593
Jyhwenchai <cai5301@qq.com>
2022-04-25 16:42:32 +0800

    add new

---
+++ RemoteBinaryPackage/Remote.swift
@@ -0,0 +1,14 @@
+//
+//  Remote.swift
+//  RemoteBinaryPackage
+//
+//  Created by 蔡志文 on 2022/4/25.
+//
+
+import Foundation
+
+public struct Remote {
+    public static func foo() {
+        print("foo...")
+    }
+}
---
+++ output/RemoteBinaryPackage.xcframework/ios-arm64_x86_64-simulator/RemoteBinaryPackage.framework/Headers/RemoteBinaryPackage.h
@@ -0,0 +1,18 @@
+//
+//  RemoteBinaryPackage.h
+//  RemoteBinaryPackage
+//
+//  Created by 蔡志文 on 2022/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteBinaryPackage.
+FOUNDATION_EXPORT double RemoteBinaryPackageVersionNumber;
+
+//! Project version string for RemoteBinaryPackage.
+FOUNDATION_EXPORT const unsigned char RemoteBinaryPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteBinaryPackage/PublicHeader.h>
+
+
